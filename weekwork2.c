3f5f2cec73b2892933cbdbd8bc2b6bc175b321bf
TENwh <695126460@qq.com>
2020-03-30 02:25:57 +0800

    work111

---
+++ weekwork2.c
@@ -0,0 +1,29 @@
+//
+//  main.c
+//  weekend1-2
+//
+//  Created by edz on 2020/3/29.
+//
+
+
+#include <stdio.h>
+
+int main(void) {
+    int i;
+    
+    int n;
+    
+    n = 1; i = 1;
+    
+    for( i = 1; i <= 10; i++)
+        
+        n = n * i;
+    printf("n的阶乘=%d\n", n);
+    
+    return 0;
+  
+    }
+    
+    
+
+     
---
+++ weekwork3.c
@@ -0,0 +1,35 @@
+//
+//  main.c
+//  weekend1-3
+//
+//  Created by edz on 2020/3/29.
+//  Copyright © 2020 twh. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    
+    int x, y;
+    
+    scanf("请输入月份:%d\n" , x);
+    
+    if(x == 2)
+        
+        y = 28;
+    printf("%d月的天数为%d\n" , x , y);
+    
+}
+if(x == 1 ;x == 3 ;x == 5 ;x = 7; x = 8; x = 10 ; x = 12)
+    
+        y = 31;
+    printf("%d月的天数为%d\n" , x , y);
+    
+}else
+        y = 30
+        printf("%d月的天数为%d\n" , x , y);
+    
+}
+    
+    return 0;
+}
