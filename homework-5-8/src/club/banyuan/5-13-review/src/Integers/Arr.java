db484a47a8749802e0adfb09d71b983313040f8d
TENwh <695126460@qq.com>
2020-05-19 10:41:17 +0800

    Review

---
+++ homework-5-8/src/club/banyuan/5-13-review/src/Integers/Arr.java
@@ -0,0 +1,19 @@
+package Integers;
+
+import java.net.SocketOption;
+import java.sql.SQLOutput;
+
+public class Arr {
+    public static void main(String[] args) {
+
+
+        int[] arr = {1, 2, 3, 4};
+        for (int i = 0; i <= arr.length; ++i) {
+            System.out.println(i);
+        }
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+
+}
